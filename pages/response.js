83a4eb2e30b1e6a0fc3710b413e7327bb5e418a3
NamanMunjal17 <116496808+NamanMunjal17@users.noreply.github.com>
2023-09-27 20:37:13 +0530

    final?

--- pages/response.js
+++ pages/response.js
@@ -26,7 +26,7 @@ import { useRef } from "react";
 export async function getServerSideProps(context) {
     
     const id = context.query.id;
-    let data = await fetch("/api/responseApi", { method: "POST", body: JSON.stringify({ "formUUID": id }) });
+    let data = await fetch("https://www.smartforms.tech/api/responseApi", { method: "POST", body: JSON.stringify({ "formUUID": id }) });
     data = await data.json();
     let q2labels={};
     let q2data={};
