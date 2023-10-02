e648f7b532be7e63b7eb3c5ed3dbc4b2e021499d
AryanK3 <78412584+AryanK3@users.noreply.github.com>
2023-10-02 19:33:02 +0530

    fixed allforms

--- pages/allforms.js
+++ pages/allforms.js
@@ -3,11 +3,31 @@ import{
     Card,
     CardContent,
     Typography,
-    Button
+    Button,
+    AppBar,
+    Menu, MenuItem,
+    Toolbar,
+    Avatar, Tooltip,
+    IconButton, ListItemIcon,
+    Link,
+    CssBaseline
 } from '@mui/material'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Logout from '@mui/icons-material/Logout'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 export default function Allforms(){
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { data: session } = useSession();
   const [forms, setForms] = useState([]);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   useEffect(() => {
     fetch('/api/getAllforms')
       .then((response) => response.json())
@@ -16,9 +36,56 @@ export default function Allforms(){
   console.log(forms)
   return (
     <div>
-      <Typography variant="h3">Latest Forms</Typography>
+      <CssBaseline/>
+        <AppBar position="static" sx={{minHeight: '5vh',maxHeight:'15vh', backgroundColor: "white"}}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'black' }}>
+            BLOcksurvVEY
+          </Typography>
+          <Button href="/howto" variant="outlined">First time?</Button>
+          {session ? (
+          <Tooltip title="Account settings">
+            <IconButton onClick={handleClick}>
+              <Avatar alt={session.user.email} src={session.user.image} />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Button onClick={() => signIn('google')} variant="outlined">Login/Signup</Button>
+        )}
+
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          sx= {{
+              overflow: 'visible',
+              mt: 1.5,
+            }}
+        >
+    
+    <Link href="/dashboard" passHref>
+      <MenuItem onClick={handleClose}>
+        <ListItemIcon>
+          <AccountCircleIcon fontSize="medium" />
+        </ListItemIcon>
+        Dashboard
+      </MenuItem>
+    </Link>
+          <MenuItem onClick={()=>signOut()}>
+            <ListItemIcon>
+              <Logout fontSize="medium" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
+        </Menu>
+    
+        </Toolbar>
+      </AppBar>
+
+      <Typography variant="h3" sx={{marginTop: '15px'}}>Latest Forms:</Typography>
       <div>
-        {forms.map((form) => (
+        {[...forms].reverse().map((form) => (
           <Card key={form.id} variant="outlined" sx={{marginBottom: '5px'}}>
             <CardContent>
               <Typography variant="h6">{form.title}</Typography>
