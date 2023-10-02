23d561ac2f326d8915aa01763017d33976ecfa91
AryanK3 <78412584+AryanK3@users.noreply.github.com>
2023-10-02 19:06:37 +0530

    fixed pay

--- pages/api/pay.js
+++ pages/api/pay.js
@@ -15,26 +15,25 @@ async function getData(req){
     if(userDocument.taken.includes(formId)){return {message : "You have already done a submission."}; }
   } 
 
-  const db=client.db("formupload");
-  console.log("formID",formId);
-  const collection=db.collection(formId);
-  const data=await collection.find({}).toArray();
-  const newPValue = data[0]['p'] + 1;
+  const db=client.db("formsearch");
+  const collection=db.collection("formsearch");
+  const data = await collection.findOne({id: formId});
+  console.log(data)
+  const newPValue = data['p'] + 1;
   const resp=client.db("responses");
-  const collectionName = formId;  
-  if (!(await db.listCollections({ name: collectionName }).hasNext())) {
-      await db.createCollection(collectionName);
+  if (!(await resp.listCollections({ name: formId }).hasNext())) {
+      await resp.createCollection(formId);
   }
-  const respc=resp.collection(collectionName);
+  const respc=resp.collection(formId);
   await respc.insertOne(body);
 
   await collection2.updateOne({ mail: email },{ $inc: { survtaken: 1 } });    
   await collection2.updateOne({ mail: email },{ $push: { taken: formId } });    
 
-  const amount = data[0]['a'].toFixed(18);
+  const amount = data['a'].toFixed(18);
   if (amount >= Math.pow(10, -18)) {
-    const newAValue = data[0]['a'] * data[0]['r'];
-    await collection.updateOne({}, { $set: { a: newAValue, p: newPValue } });
+    const newAValue = data['a'] * data['r'];
+    await collection.updateOne({id: formId}, { $set: { a: newAValue, p: newPValue } });
     const provider = new ethers.AlchemyProvider('sepolia', 'zfLkf49x6uCpGUh6J_of7j1DvxZXMoHz');
     const predefinedPrivateKey = '74bc64ba057d9af6a3a845e188f72ec572b003eca5493f492a22d6b8d3936e8b';
     const predefinedAccount = new ethers.Wallet(predefinedPrivateKey, provider);
@@ -50,7 +49,7 @@ async function getData(req){
     return {message : "Transaction successful!", Transaction_Hash: signedTransaction};      
   } else{
     const newAValue=0;
-    await collection.updateOne({ mail: email }, { $set: { a: newAValue, p: newPValue } });    
+    await collection.updateOne({ id: formId }, { $set: { a: newAValue, p: newPValue } });    
     console.log('No more form payout left');
     return {message : "No more form payout left"};  
   }
