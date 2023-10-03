8575ac194f7496d316be88fa0cc2ca6e61129713
AryanK3 <78412584+AryanK3@users.noreply.github.com>
2023-10-03 17:40:27 +0530

    '

--- pages/api/findForm.js
+++ pages/api/findForm.js
@@ -2,9 +2,8 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
     const client = await clientPromise;
-    const db = client.db("formupload");
+    const db = client.db("formsearch");
     const body = JSON.parse(req.body);
-    const collection = body.id;
-    const dat = await db.collection(collection).find({}).toArray();
+    const dat = await db.collection("formsearch").findOne({id : body.id});
     res.json({ "data": dat });
 }
